f9e812068cb9dfdc2f6d6e7e089881b7e6abbada
amaraSanoh <sanohawa@gmail.com>
2020-03-10 14:23:23 +0100

    modifier un utilisateur

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/interfaces/User';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogModifUserComponent } from '../../page/dialogues/dialog-modif-user/dialog-modif-user.component';
+import { ListUsersService } from 'src/app/service/list-users.service';
 
 @Component({
   selector: 'app-card',
@@ -8,14 +11,42 @@ import { User } from 'src/app/interfaces/User';
 })
 export class CardComponent implements OnInit {
 
+  private addDialog: MatDialogRef<DialogModifUserComponent>;
+  dialogStatus: boolean = false;
+
   @Input()
   user: User;
 
-  constructor() {
+  constructor(public dialog: MatDialog, private listUsersService: ListUsersService) {
     // this.user = null;
   }
 
   ngOnInit() {
+  }
+
+  editUser(user: User) {
+    this.listUsersService.editUser(user);
+    this.hideDialog();
+  }
+
+  showDialog(user: User) {
+    this.dialogStatus = true;
+    this.addDialog = this.dialog.open(DialogModifUserComponent, {
+      width: '600px',
+      data: { userObject: user }
+    });
+
+    this.addDialog.afterClosed().subscribe(user => {
+      this.dialogStatus = false;
+      if (user) {
+        this.editUser(user);
+      }
+    });
+  }
+
+  hideDialog() {
+    this.dialogStatus = false;
+    this.addDialog.close();
   }
 
 }
